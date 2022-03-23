13fcca152ff40fe55a6b19480eb4ef38e46142af
cladg92 <cladg92@gmail.com>
2022-03-23 18:49:35 +0100

    Add first 3 Pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
+let pokemonList = [
+    {name:'Bulbasaur', height: 4, types:['grass','poison'] },
+    {name:'Venusaur', height: 2, types:['fairy','ice','flying']},
+    {name:'Weedle', height: 0.3, types:['bug','poison','fire']}
+];
+console.log(pokemonList);
 
-let favoriteFood='Artichoke';
-document.write(favoriteFood);
+
+
