e90f08945a6b036b89b8fbacde26a414b533f15a
cladg92 <cladg92@gmail.com>
2022-03-24 12:20:03 +0100

    List Pokemon names and height in document

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
 let pokemonList = [
-    {name:'Bulbasaur', height: 4, types:['grass','poison'] },
-    {name:'Venusaur', height: 2, types:['fairy','ice','flying']},
-    {name:'Weedle', height: 0.3, types:['bug','poison','fire']}
+    {
+        name:'Bulbasaur',
+        height: 4,
+        types:['grass','poison'] 
+    },
+    {
+        name:'Venusaur',
+        height: 2, 
+        types:['fairy','ice','flying']
+    },
+    {
+        name:'Weedle', 
+        height: 0.3, 
+        types:['bug','poison','fire']
+    }
 ];
 console.log(pokemonList);
+
+
+// List Pokemon names and height in document
+for(i=0; i<pokemonList.length; i++){
+    if(pokemonList[i].height>3){
+        document.write(`<p><span class=name>${pokemonList[i].name}</span> (height: ${pokemonList[i].height}). <span class="tall_pokemon"> Wow, that's tall!</span></p>`);
+    } else{
+        document.write(`<p><span class="name">${pokemonList[i].name}</span> (height: ${pokemonList[i].height}).</p>`)
+    }
+}
 
 
 
