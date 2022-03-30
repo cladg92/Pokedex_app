2903345c4f6d392b4bd6a6c4877b10e4ceade343
cladg92 <cladg92@gmail.com>
2022-03-30 11:47:32 +0200

    Add forEach loop to Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,20 @@ let pokemonList = [
 console.log(pokemonList);
 
 
+
 // List Pokemon names and height in document
-for(i=0; i<pokemonList.length; i++){
+pokemonList.forEach(function(item){
+    if(item.height>3){
+        document.write(`<p><span class=name>${item.name}</span> (height: ${item.height}). <span class="tall_pokemon"> Wow, that's tall!</span></p>`);
+    } else{
+        document.write(`<p><span class="name">${item.name}</span> (height: ${item.height}).</p>`)
+    }
+})
+
+/* for(i=0; i<pokemonList.length; i++){
     if(pokemonList[i].height>3){
         document.write(`<p><span class=name>${pokemonList[i].name}</span> (height: ${pokemonList[i].height}). <span class="tall_pokemon"> Wow, that's tall!</span></p>`);
     } else{
         document.write(`<p><span class="name">${pokemonList[i].name}</span> (height: ${pokemonList[i].height}).</p>`)
     }
-}
+} */
