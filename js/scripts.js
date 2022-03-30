829578e78e0d19a345fa747d1b2dc4a61d5aad55
cladg92 <cladg92@gmail.com>
2022-03-30 15:18:30 +0200

    Add IIFE function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,47 @@
-let pokemonList = [
-    {
-        name:'Bulbasaur',
-        height: 4,
-        types:['grass','poison'] 
-    },
-    {
-        name:'Venusaur',
-        height: 2, 
-        types:['fairy','ice','flying']
-    },
-    {
-        name:'Weedle', 
-        height: 0.3, 
-        types:['bug','poison','fire']
-    }
-];
-console.log(pokemonList);
+let pokemonRepository = (function (){
+    let pokemonList = [
+        {
+            name:'Bulbasaur',
+            height: 4,
+            types:['grass','poison'] 
+        },
+        {
+            name:'Venusaur',
+            height: 2, 
+            types:['fairy','ice','flying']
+        },
+        {
+            name:'Weedle', 
+            height: 0.3, 
+            types:['bug','poison','fire']
+        }
+    ];
 
+    function getAll(){
+        return pokemonList;
+    }
+
+    function add (pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+
+// access pokemonList
+console.log(pokemonRepository.getAll());
+
+// add pokemon to pokemonList
+pokemonRepository.add({name: 'Dot', height: 10, types:['cuddle', 'ice']});
+console.log(pokemonRepository.getAll());
 
 
 // List Pokemon names and height in document
-pokemonList.forEach(function(item){
+pokemonRepository.getAll().forEach(function(item){
     if(item.height>3){
         document.write(`<p><span class=name>${item.name}</span> (height: ${item.height}). <span class="tall_pokemon"> Wow, that's tall!</span></p>`);
     } else{
