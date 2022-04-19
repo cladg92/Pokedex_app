94f263345f5674059198fe30f835b5b5e60820c8
cladg92 <cladg92@gmail.com>
2022-04-19 15:22:33 +0200

    Implement search/filter bar

--- js/scripts.mjs
+++ js/scripts.mjs
@@ -128,45 +128,64 @@ let pokemonRepository = (function () {
 
     }
 
-        // Show Details
-        function showDetails(pokemon) {
-          loadDetails(pokemon).then(function () {
-            showModal(pokemon)});
-        }
+    // Show Details
+    function showDetails(pokemon) {
+      loadDetails(pokemon).then(function () {
+        showModal(pokemon)});
+    }
 
-        // sort pokemon List
-        function sortList() {
-          var list, i, switching, b, shouldSwitch;
-          list = document.getElementById('pokemon-list');
-          switching = true;
-          /* Make a loop that will continue until
-          no switching has been done: */
-          while (switching) {
-            // start by saying: no switching is done:
-            switching = false;
-            b = list.getElementsByTagName('LI');
-            // Loop through all list-items:
-            for (i = 0; i < (b.length - 1); i++) {
-              // start by saying there should be no switching:
-              shouldSwitch = false;
-              /* check if the next item should
-              switch place with the current item: */
-              if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-                /* if next item is alphabetically
-                lower than current item, mark as a switch
-                and break the loop: */
-                shouldSwitch = true;
-                break;
-              }
-            }
-            if (shouldSwitch) {
-              /* If a switch has been marked, make the switch
-              and mark the switch as done: */
-              b[i].parentNode.insertBefore(b[i + 1], b[i]);
-              switching = true;
-            }
+    // sort pokemon List
+    function sortList() {
+      var list, i, switching, b, shouldSwitch;
+      list = document.getElementById('pokemon-list');
+      switching = true;
+      /* Make a loop that will continue until
+      no switching has been done: */
+      while (switching) {
+        // start by saying: no switching is done:
+        switching = false;
+        b = list.getElementsByTagName('LI');
+        // Loop through all list-items:
+        for (i = 0; i < (b.length - 1); i++) {
+          // start by saying there should be no switching:
+          shouldSwitch = false;
+          /* check if the next item should
+          switch place with the current item: */
+          if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+            /* if next item is alphabetically
+            lower than current item, mark as a switch
+            and break the loop: */
+            shouldSwitch = true;
+            break;
           }
         }
+        if (shouldSwitch) {
+          /* If a switch has been marked, make the switch
+          and mark the switch as done: */
+          b[i].parentNode.insertBefore(b[i + 1], b[i]);
+          switching = true;
+        }
+      }
+    }
+
+
+    // Function to search and filter Pokemons
+    function searchPokemon() {
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("pokemon-list");
+      li = ul.getElementsByTagName("li");
+      for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("button")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+          } else {
+              li[i].style.display = "none";
+          }
+      }
+  }
 
 
       return {
@@ -176,7 +195,8 @@ let pokemonRepository = (function () {
         loadList: loadList,
         loadDetails: loadDetails,
         showDetails: showDetails,
-        sortList: sortList
+        sortList: sortList,
+        searchPokemon: searchPokemon
       };
     })();
 
