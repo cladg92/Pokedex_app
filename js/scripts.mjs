afc689fd9237ddebb05fc0ad13b26480fe09505a
cladg92 <cladg92@gmail.com>
2022-04-19 09:58:31 +0200

    Implement sortList() function

--- js/scripts.mjs
+++ js/scripts.mjs
@@ -134,13 +134,49 @@ let pokemonRepository = (function () {
             showModal(pokemon)});
         }
 
+        // sort pokemon List
+        function sortList() {
+          var list, i, switching, b, shouldSwitch;
+          list = document.getElementById('pokemon-list');
+          switching = true;
+          /* Make a loop that will continue until
+          no switching has been done: */
+          while (switching) {
+            // start by saying: no switching is done:
+            switching = false;
+            b = list.getElementsByTagName('LI');
+            // Loop through all list-items:
+            for (i = 0; i < (b.length - 1); i++) {
+              // start by saying there should be no switching:
+              shouldSwitch = false;
+              /* check if the next item should
+              switch place with the current item: */
+              if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                /* if next item is alphabetically
+                lower than current item, mark as a switch
+                and break the loop: */
+                shouldSwitch = true;
+                break;
+              }
+            }
+            if (shouldSwitch) {
+              /* If a switch has been marked, make the switch
+              and mark the switch as done: */
+              b[i].parentNode.insertBefore(b[i + 1], b[i]);
+              switching = true;
+            }
+          }
+        }
+
+
       return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
         loadList: loadList,
         loadDetails: loadDetails,
-        showDetails: showDetails
+        showDetails: showDetails,
+        sortList: sortList
       };
     })();
 
@@ -155,4 +191,10 @@ pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
   });
+
+// sort Pokemon List
+pokemonRepository.sortList();
+
+
+  
 });
