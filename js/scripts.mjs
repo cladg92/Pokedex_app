35860d15a847fed78439180031cc1c150bb772c4
cladg92 <cladg92@gmail.com>
2022-04-19 16:06:31 +0200

    ESLint errors correct

--- js/scripts.mjs
+++ js/scripts.mjs
@@ -172,17 +172,17 @@ let pokemonRepository = (function () {
     // Function to search and filter Pokemons
     function searchPokemon() {
       var input, filter, ul, li, a, i, txtValue;
-      input = document.getElementById("myInput");
+      input = document.getElementById('myInput');
       filter = input.value.toUpperCase();
-      ul = document.getElementById("pokemon-list");
-      li = ul.getElementsByTagName("li");
+      ul = document.getElementById('pokemon-list');
+      li = ul.getElementsByTagName('li');
       for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("button")[0];
+          a = li[i].getElementsByTagName('button')[0];
           txtValue = a.textContent || a.innerText;
           if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              li[i].style.display = "";
+              li[i].style.display = '';
           } else {
-              li[i].style.display = "none";
+              li[i].style.display = 'none';
           }
       }
   }
